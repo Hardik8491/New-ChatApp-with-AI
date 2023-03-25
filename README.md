e0a09d2344c7098bb70e6b0aa5b28c1b8d32e55e
Hardik Bhammar <94770400+Hardik8491@users.noreply.github.com>
2023-03-25 19:24:51 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# New-ChatApp-with-AI
---
+++ chatapp-becend/index.js
@@ -0,0 +1,50 @@
+const express = require("express");
+const cors = require("cors");
+const axios = require("axios");
+
+const app = express();
+app.use(express.json());
+app.use(cors({ origin: true }));
+
+const CHAT_ENGINE_PROJECT_ID = "0d40d730-a55d-4c89-9b00-fe16770b2fd1";
+const CHAT_ENGINE_PRIVATE_KEY = "513712fc-877d-4a8b-9429-2c65fb6e4197";
+
+app.post("/signup", async (req, res) => {
+  const { username, secret, email, first_name, last_name } = req.body;
+
+  // Store a user-copy on Chat Engine!
+  // Docs at rest.chatengine.io
+  try {
+    const r = await axios.post(
+      "https://api.chatengine.io/users/",
+      { username, secret, email, first_name, last_name },
+      { headers: { "Private-Key": CHAT_ENGINE_PRIVATE_KEY } }
+    );
+    return res.status(r.status).json(r.data);
+  } catch (e) {
+    return res.status(e.response.status).json(e.response.data);
+  }
+});
+
+app.post("/login", async (req, res) => {
+  const { username, secret } = req.body;
+
+  // Fetch this user from Chat Engine in this project!
+  // Docs at rest.chatengine.io
+  try {
+    const r = await axios.get("https://api.chatengine.io/users/me/", {
+      headers: {
+        "Project-ID": CHAT_ENGINE_PROJECT_ID,
+        "User-Name": username,
+        "User-Secret": secret,
+      },
+    });
+    return res.status(r.status).json(r.data);
+  } catch (e) {
+    return res.status(e.response.status).json(e.response.data);
+  }
+});
+
+// vvv On port 3001!
+app.listen(3001);
+console.log("server start on  : localhost:3001/")
